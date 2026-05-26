725d3ccee532efe8e64fa3d8c9543602271b9999
Eclipsia <novahaumea@proton.me>
2026-05-26 16:43:43 +0600

    Update CineStreamProvider.kt

--- CineStream/src/main/kotlin/com/eclipsia/CineStreamProvider.kt
+++ CineStream/src/main/kotlin/com/eclipsia/CineStreamProvider.kt
@@ -1,4 +1,4 @@
-package com.megix
+package com.eclipsia
 
 // Cloudstream Core, Utils, & Logging
 import com.lagradost.api.Log
@@ -24,9 +24,9 @@ import kotlinx.coroutines.runBlocking
 // JSON Parsing
 import org.json.JSONObject
 
-import com.megix.CineStreamExtractors.invokeAllSources
-import com.megix.CineStreamExtractors.invokeAllAnimeSources
-import com.megix.CineStreamExtractors.invokeAnimes
+import com.eclipsia.CineStreamExtractors.invokeAllSources
+import com.eclipsia.CineStreamExtractors.invokeAllAnimeSources
+import com.eclipsia.CineStreamExtractors.invokeAnimes
 
 open class CineStreamProvider : MainAPI() {
     override var mainUrl = "https://cinemeta-catalogs.strem.io"
@@ -53,26 +53,7 @@ open class CineStreamProvider : MainAPI() {
     override val mainPage = mainPageOf(
         "$mainUrl/top/catalog/movie/top/skip=###" to "Top Movies",
         "$mainUrl/top/catalog/series/top/skip=###" to "Top Series",
-        "$aiometa_url/catalog/anime/mal.airing/skip=###" to "Top Airing Anime",
         "$kitsu_url/catalog/anime/kitsu-anime-trending/skip=###" to "Top Anime",
-        "$mainUrl/top/catalog/movie/top/skip=###&genre=Action" to "Top Action Movies",
-        "$mainUrl/top/catalog/series/top/skip=###&genre=Action" to "Top Action Series",
-        "$mainUrl/top/catalog/movie/top/skip=###&genre=Comedy" to "Top Comedy Movies",
-        "$mainUrl/top/catalog/series/top/skip=###&genre=Comedy" to "Top Comedy Series",
-        "$mainUrl/top/catalog/movie/top/skip=###&genre=Romance" to "Top Romance Movies",
-        "$mainUrl/top/catalog/series/top/skip=###&genre=Romance" to "Top Romance Series",
-        "$mainUrl/top/catalog/movie/top/skip=###&genre=Horror" to "Top Horror Movies",
-        "$mainUrl/top/catalog/series/top/skip=###&genre=Horror" to "Top Horror Series",
-        "$mainUrl/top/catalog/movie/top/skip=###&genre=Thriller" to "Top Thriller Movies",
-        "$mainUrl/top/catalog/series/top/skip=###&genre=Thriller" to "Top Thriller Series",
-        "$mainUrl/top/catalog/movie/top/skip=###&genre=Sci-Fi" to "Top Sci-Fi Movies",
-        "$mainUrl/top/catalog/series/top/skip=###&genre=Sci-Fi" to "Top Sci-Fi Series",
-        "$mainUrl/top/catalog/movie/top/skip=###&genre=Fantasy" to "Top Fantasy Movies",
-        "$mainUrl/top/catalog/series/top/skip=###&genre=Fantasy" to "Top Fantasy Series",
-        "$mainUrl/top/catalog/movie/top/skip=###&genre=Mystery" to "Top Mystery Movies",
-        "$mainUrl/top/catalog/series/top/skip=###&genre=Mystery" to "Top Mystery Series",
-        "$mainUrl/top/catalog/movie/top/skip=###&genre=Crime" to "Top Crime Movies",
-        "$mainUrl/top/catalog/series/top/skip=###&genre=Crime" to "Top Crime Series",
     )
 
     private fun getPosterUrl(
